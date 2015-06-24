5e5ec8d37b4041a27f0d477a083ceebb52bc7350
Nacaal <nacaal@outlook.com>
2015-06-24 11:25:58 +0200

    new project "Composition.AutoFac"

---
+++ Source/Composition.AutoFac/Model/Apple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetVortex.Composition.AutoFac.Model
+{
+    class Apple : Fruit, ICitrus
+    {
+        public int SourLevel { get; set; }
+    }
+}
---
+++ Source/Composition.AutoFac/Program.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetVortex.Composition.AutoFac
+{
+    class Program
+    {
+        private static IContainer _container;
+
+        static void Main(string[] args)
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            _container = builder.Build();
+        }
+    }
+}
