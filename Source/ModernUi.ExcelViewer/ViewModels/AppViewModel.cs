e5a8787cafba58928498e0e72b3fa41fedb61011
Nacaal <nacaal@outlook.com>
2015-06-25 11:28:36 +0200

    Lost files

---
+++ Source/ModernUi.ExcelViewer/ViewModels/AppViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.Windows;
+using Caliburn.Micro;
+using Microsoft.Win32;
+using NetVortex.Office.SimpleExcelXml;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetVortex.ModernUi.ExcelViewer.Model;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.IO;
+
+namespace NetVortex.ModernUi.ExcelViewer.ViewModels
+{
+    class AppViewModel : PropertyChangedBase, IHaveDisplayName
+    {
+        public BindableCollection<SheetViewModel> Sheets { get; private set; }
+        public string DisplayName { get; set; }
+
+        public AppViewModel()
+        {
+            DisplayName = "Excel Viewer";
+            Sheets = new BindableCollection<SheetViewModel>();
+            Sheets.CollectionChanged += (e, s) => NotifyOfPropertyChange(() => CanCreateSql);
+        }
+
+        public async void CreateSql()
+        {
+            var mainWin = Application.Current.MainWindow as MetroWindow;
+
+            if (mainWin == null)
+                return;
+
+            var progressController = await mainWin.ShowProgressAsync("SQL generation", "Exporting Excel file to SQL...");
+
+            var sqlBuilder = new StringBuilder();
+
+            const string baseSql = "UPDATE [dbo].[Account_AccountLib_{0}] SET [ITManagerKonto] = '{1}' " +
+                                   "WHERE [MitarbeiterUri] = '{2}' AND [ITManagerKonto] = '{3}' " +
+                                   "AND [Status] = 'deleted'";
+
+            sqlBuilder.AppendLine("USE [comet3db]").AppendLine()
+                      .AppendLine("BEGIN TRANSACTION");
+
+            foreach (var sheet in Sheets)
+            {
+                sqlBuilder.AppendLine().AppendFormat("-- {0}", sheet.DisplayName.ToUpper()).AppendLine();
+                for (int i = 1; i < sheet.SheetRows.Count; i++)
+                {
+                    var row = sheet.SheetRows[i];
+                    sqlBuilder.AppendFormat(baseSql, sheet.DisplayName, row.ItManagerNeu, row.MitarbeiterUri, row.ItManagerAlt).AppendLine();
+                }
+            }
+
+            sqlBuilder.AppendLine().AppendLine("COMMIT");
+
+            var saveDialog = new SaveFileDialog();
+            var dialogResult = saveDialog.ShowDialog();
+
+            if (dialogResult.HasValue && dialogResult.Value)
+            {
+                using (var streamWriter = new StreamWriter(File.OpenWrite(saveDialog.FileName)))
+                    await streamWriter.WriteAsync(sqlBuilder.ToString());
+            }
+
+            await progressController.CloseAsync();
+        }
+
+        public bool CanCreateSql
+        {
+            get { return Sheets != null && Sheets.Any(); }
+        }
+
+        public async void LoadFile()
+        {
+            var fileDialog = new OpenFileDialog() { Filter = "Excel Files|*.xls;*.xlsx", Multiselect = false };
+
+            var result = fileDialog.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                Sheets.Clear();
+                string file = fileDialog.FileName;
+
+                dynamic excelObj = await ExcelParser.GetExcelAsync();
+                
+                await Task.Run(() => excelObj.Workbooks.Open(file));
+                await Task.Run(() =>
+                {
+                    foreach (var sheetObj in excelObj.ActiveWorkbook.Sheets)
+                    {
+                        Sheet sheet = new Sheet(sheetObj.Name);
+                        object[,] sheetValues = sheetObj.UsedRange.Value;
+
+                        if (sheetValues == null || (sheetValues.GetUpperBound(1) - sheetValues.GetLowerBound(1) < 4))
+                            break;
+
+                        for (int i = sheetValues.GetLowerBound(0); i <= sheetValues.GetUpperBound(0); i++)
+                        {
+                            SheetRow row = new SheetRow()
+                            {
+                                ItManagerAlt = sheetValues[i, 1] as string,
+                                MitarbeiterUri = sheetValues[i, 2] as string,
+                                DienstName = sheetValues[i, 3] as string,
+                                DisplayName = sheetValues[i, 4] as string,
+                                ItManagerNeu = sheetValues[i, 5] as string
+                            };
+
+                            sheet.Add(row);
+                        }
+
+                        Sheets.Add(new SheetViewModel(sheet));
+                    }
+                });
+
+                excelObj.Application.Quit();
+            }
+        }
+    }
+}
---
+++ Source/Office.SimpleExcelXml/ExcelParser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NetVortex.Office.SimpleExcelXml
+{
+    public class ExcelParser
+    {
+        private const string EXCEL_PROGID = "Excel.Application";
+
+        public static Func<dynamic> GetExcel = () => Activator.CreateInstance(Type.GetTypeFromProgID(EXCEL_PROGID));
+
+        public static Func<Task<dynamic>> GetExcelAsync = async ()
+            => await Task.Run(() => Activator.CreateInstance(Type.GetTypeFromProgID(EXCEL_PROGID)))
+                .ConfigureAwait(false);
+
+        public FileInfo ExcelFile { get; private set; }
+
+        public ExcelParser(string file)
+        {
+            if (!File.Exists(file))
+                throw new ArgumentException(string.Format("Error opening {0}. File not found.", file));
+
+            ExcelFile = new FileInfo(file);
+        }
+
+        public async Task<XElement> ParseXmlAsync(string file)
+        {
+            var resultXml = new XElement("Workbook", new XAttribute("File", ExcelFile.Name));
+
+            var excel = await GetExcelAsync();
+            excel.Application.Workbooks.Open(ExcelFile);
+            dynamic workBook = excel.ActiveWorkbook;
+
+            foreach (var sheet in workBook.Sheets)
+            {
+                var sheetXml = new XElement("Sheet", new XAttribute("Name", sheet.Name));
+
+                var sheetRange = sheet.UsedRange;
+                object[,] sheetValues = sheetRange.Value;
+
+                for (int i = sheetValues.GetLowerBound(0); i < sheetValues.GetUpperBound(0); i++)
+                {
+                    XElement rowXml = new XElement("Row");
+                    for (int j = sheetValues.GetLowerBound(1); j < sheetValues.GetUpperBound(1); j++)
+                    {
+                        rowXml.Add(new XElement("Column" + j, sheetValues[i, j]));
+                    }
+                    sheetXml.Add(rowXml);
+                }
+
+                resultXml.Add(sheetXml);
+            }
+
+            excel.Application.Quit();
+
+            return resultXml;
+        }
+    }
+}
+
